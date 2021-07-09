f84e6ad7e082af1d503e969604179b09f945a778
Shailendra Shukla <shailendrashuklapro@gmail.com>
2021-07-09 00:32:28 -0500

    feat: add theme

---
+++ too/src/plugins/theme.js
@@ -0,0 +1,12 @@
+import colors from 'vuetify/lib/util/colors'
+
+export default {
+  primary: {
+    base: colors.purple.base,
+    darken1: colors.purple.darken2,
+  },
+
+  secondary: colors.indigo,
+ 
+  tertiary: colors.pink.base,
+}
